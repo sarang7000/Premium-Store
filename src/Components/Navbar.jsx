83365460e3e0941dd6aa1cf50af30de0835096c7
Sarang Patel <sarangsakariya1999@gmail.com>
2023-09-06 15:41:31 +0530

    Check

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { UseSelector, useSelector } from "react-redux";
 const Navbar = () => {
     const state = useSelector((state)=>state.handleCart)
     return (
-        <div> 
+        <div>       
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
                 <div className="container">
                     <NavLink className="navbar-brand fw-bold fs-4" to="/">Premium Store</NavLink>
