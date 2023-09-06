b4a7eb9045a47392631a07659a5888b375fc8e01
Sarang Patel <sarangsakariya1999@gmail.com>
2023-09-06 15:23:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import { Routes, Route } from 'react-router-dom';
+import Products from './Components/Products';
+import Product from './Components/Product';
+import Cart from './Components/cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Navbar />
+        <Cart/>
+        <Routes>
+          <Route exact path="/" Component={Home}/>
+          <Route exact path="/Products" Component={Products}/>
+          <Route exact path="/Products/:id" Component={Product}/>
+        </Routes>
+      </>
   );
 }
 
---
+++ src/Components/cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { UseSelector } from 'react-redux';
+
+const Cart=()=>{
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Cart;
